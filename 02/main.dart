bd5a084a18794b41b3e08ce6b646b30a576decc8
rinukkusu <rinukkusu@sub-r.de>
2017-12-04 21:25:40 +0100

    add code for day 2 :)

---
+++ 02/main.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  var sum = new File('data')
+      .readAsLinesSync()
+      .map((x) => x
+          .split(new RegExp(r'\s'))
+          .map((i) => int.parse(i))
+          .toList()
+          ..sort())
+      .map((List<int> x) => x.last - x.first)
+      .toList()
+      .reduce((x, y) => x + y);
+
+  print(sum);
+}
